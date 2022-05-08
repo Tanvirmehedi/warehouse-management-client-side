1789e0fd2ad784ccd6a5cc14954535d514b12bf8
Tanvirmehedi <tjmehedi201@gmail.com>
2022-05-09 03:01:31 +0600

    Navabar Upddate

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -126,12 +126,16 @@ export default function Navbar({ fixed }) {
           </div>
 
           <div className="flex items-center relative">
-            <CustomLink
-              className="text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-4"
-              to="/login"
-            >
-              Login
-            </CustomLink>
+            {user ? (
+              ""
+            ) : (
+              <CustomLink
+                className="text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-4"
+                to="/login"
+              >
+                Login
+              </CustomLink>
+            )}
 
             <div className="dropdown relative">
               <CustomLink
